80047fdf46833a9377e748c3593176a50d2e9178
Daniel Potts <Daniel.Potts@mkodo.com>
2024-03-21 11:35:42 +0000

    added moving balls

--- src/components/MovingBall.tsx
+++ src/components/MovingBall.tsx
@@ -1,15 +1,14 @@
-import "./LoadingSpinner.css";
+import React from 'react';
+import "./MovingBall.css";
 
-function MovingBall() {
+function MovingBall({ leftPosition }: { leftPosition: number }) {
     const randomIntFromInterval = (min: number, max: number) => {
         return Math.floor(Math.random() * (max - min + 1) + min)
-      }
-      
-  return (
-    <div className="container">
-    <div className="ball bg-red">{randomIntFromInterval(1, 40)}</div>
-  </div>
-  );
+    }
+
+    return (
+        <div className="ball bg-red" style={{ left: `${leftPosition}px` }}>{randomIntFromInterval(1, 40)}</div>
+    );
 }
 
 export default MovingBall;
--- src/pages/LiveDraw.tsx
+++ src/pages/LiveDraw.tsx
@@ -1,13 +1,32 @@
-function LiveDraw() {
-    return (
-      <div className="App">
-        <header className="App-header">
+import React, { useEffect, useState } from 'react';
+import MovingBall from '../components/MovingBall';
 
-        </header>
-        <div className=""></div>
-      </div>
+function LiveDraw() {
+    const [positions, setPositions] = useState<number[]>([]);
+
+    useEffect(() => {
+        const randomIntFromInterval = (min: number, max: number) => {
+            return Math.floor(Math.random() * (max - min + 1) + min)
+        }
+
+        const newPositions = Array.from({ length: 40 }, () => randomIntFromInterval(0, window.innerWidth / 2));
+
+        newPositions.forEach((pos, i) => {
+            setTimeout(() => {
+                setPositions(prevPositions => [...prevPositions, pos]);
+            }, i * 100); // Change this value to adjust the interval
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="flex justify-center items-center h-screen">
+                <div className="w-1/2 h-64 rounded-full flex flex-wrap justify-center items-center bg-blue-200 relative">
+                    {positions.map((pos, i) => <MovingBall key={i} leftPosition={pos} />)}
+                </div>
+            </div>
+        </div>
     );
-  }
-  
-  export default LiveDraw;
-  
+}
+
+export default LiveDraw;
