702c84ac792e2f394cc878dd166cd20676a7d785
SophieGlanville01 <sophie.glanville@mkodo.com>
2024-03-21 11:20:18 +0000

    Added live draw link to results and created live draw page

--- src/pages/LiveDraw.tsx
+++ src/pages/LiveDraw.tsx
@@ -1,10 +1,8 @@
 function LiveDraw() {
     return (
-      <div className="App">
-        <header className="App-header">
-
-        </header>
-        <div className=""></div>
+      <div className="App h-screen">
+        <div className="bg-white h-3/5 justify-center text-lg">test</div>
+          <div className="bg-black h-2/5 justify-center text-lg">test2</div>
       </div>
     );
   }
--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,10 +1,15 @@
+import {Link} from "react-router-dom";
+
 function Results() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
+      <header className="bg-white flex flex-col h-screen justify-center text-lg">
+        <p className={"text-6xl font-bold text-black"}>
          Results
         </p>
+          <Link to="/live-draw">
+              <button className={"text-black hover:text-imperial"}>Live Draw</button>
+          </Link>
       </header>
     </div>
   );
