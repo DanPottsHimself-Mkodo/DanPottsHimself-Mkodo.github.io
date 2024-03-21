122f66e8d55a819719fb42314aba0335f9c6d21f
matthewp <matthew.pollard@mkodo.com>
2024-03-21 13:38:38 +0000

    Added cross button to close scanner

--- src/pages/Scan.tsx
+++ src/pages/Scan.tsx
@@ -1,10 +1,17 @@
 import TicketScanner from "../components/Scanner";
+import CrossIcon from "../icons/CrossIcon";
+import {useNavigate} from "react-router-dom";
 
 function Scan() {
+    const navigate = useNavigate();
   return (
     <div className={"flex flex-col bg-white h-screen"}>
-        <header className={"flex w-full h-16 items-center text-center"}>
-            <h1 className={"font-black text-2xl w-full text-black"}>Ticket Scanner</h1>
+        <header className={"flex w-full h-16 items-center text-center px-4"}>
+            <div className={"w-8"} />
+            <h1 className={"font-black text-2xl w-full text-black flex-grow"}>Ticket Scanner</h1>
+            <button className={"flex justify-center items-center w-8 h-8"} onClick={() => navigate("/")}>
+                <CrossIcon />
+            </button>
         </header>
         <TicketScanner onScan={(ticketId) => alert(ticketId)} />
     </div>
